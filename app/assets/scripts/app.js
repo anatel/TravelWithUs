fbfba6abeece20b699b0d8f9ab32ad3feb276ac3
Anat <anat.eliahu@gmail.com>
2017-02-04 18:31:45 +0200

    Add a modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,12 +2,11 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
-<<<<<<< HEAD
 import Modal from './modules/Modal';
-=======
->>>>>>> sticky-header
+
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
